d62afb7706b6187cfdda64b70e0e1561a31151a1
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-09 05:56:13 -0500

    Add main

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const app = require('./backend/server/server');
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get(/\/(?!graphql)*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
