ae9a14d6ae29f2b849620b50ed1334756d0304a7
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-09 06:42:30 -0500

    Fix port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const app = require('./backend/server/server');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1337;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
