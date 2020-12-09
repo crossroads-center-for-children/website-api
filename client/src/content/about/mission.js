6681590df22a34282d1c2f747d676cab469d6073
Matt Ramotar <matt.ramotar@jhu.edu>
2020-12-09 09:21:40 -0500

    Restructure

--- client/src/content/about/mission.js
+++
@@ -1,4 +0,0 @@
-const mission =
-  'To prepare individuals with and without developmental disabilities for life success within their family and community';
-
-export default mission;
--- client/src/content/about/vision.js
+++
@@ -1,3 +0,0 @@
-const vision = 'Every student we serve will achieve a full and productive life';
-
-export default vision;
